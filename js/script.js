9db4d7a33403b59e7d28a69f088c96f473b83be0
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-28 14:35:13 +0100

    add more story to the storyline

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let adjective = (prompt("any adjective of your choice"));
 
 function madlips(){
 
-    let storyline = " Shhhh! dont " + verb + "  " + " anyone -- its a secret!. We are " + verb + " " + " a suprise " + noun + " " + " for my " + adjective + " " + "friend. " ;
+    let storyline = " Shhhh! dont " + verb + "  " + " anyone -- its a secret!. We are " + verb + " " + " a suprise " + noun + " " + " for my " + adjective + " " + "friend. " + " Here is how a " + adjective + " " + " party works: First, ask guests to " + verb + " " + " a " + adjective + " " + " gift and to arrive early so they dont " + verb + " " + " the suprise. ";
 
     return storyline;
 

074493615a440030530148569dcabbe007dfa406
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-28 14:25:39 +0100

    create a function to return a story for madlips

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// Madlips 
+
+let noun = (prompt("write any noun of your choice"));
+let verb = (prompt("write any verb of your choice"));
+let adjective = (prompt("any adjective of your choice"));
+
+function madlips(){
+
+    let storyline = " Shhhh! dont " + verb + "  " + " anyone -- its a secret!. We are " + verb + " " + " a suprise " + noun + " " + " for my " + adjective + " " + "friend. " ;
+
+    return storyline;
+
+
+}
+console.log(madlips());
